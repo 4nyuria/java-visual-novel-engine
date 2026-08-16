6b1d6ee5282be52e4a4855304ffb9286644a48d5
Anyuria <giuganmo@gmail.com>
2026-08-16 07:54:06 -0300

    agregando items package y en gamestate

--- src/main/java/com/anyuria/vnengine/state/GameState.java
+++ src/main/java/com/anyuria/vnengine/state/GameState.java
@@ -2,21 +2,25 @@ package com.anyuria.vnengine.state;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import com.anyuria.vnengine.item.Item;
 
 public class GameState {
-
+	private List<Item> inventory;
+	
     private Map<String, Boolean> flags;
 
     public GameState() {
-
+    	
+    	inventory = new ArrayList<>();
         flags = new HashMap<>();
     }
-
+    //flags
     public void setFlag(String name, boolean value) {
 
         flags.put(name, value);
     }
-
     public boolean getFlag(String name) {
 
         return flags.getOrDefault(name, false);
@@ -30,5 +34,30 @@ public class GameState {
     public void removeFlag(String name) {
 
         flags.remove(name);
+    }
+    
+    //items
+    public void addItem(Item item) {
+
+        inventory.add(item);
+    }
+    public void removeItem(Item item) {
+
+        inventory.remove(item);
+    }
+    public boolean hasItem(String itemId) {
+
+        for (Item item : inventory) {
+
+            if (item.getId().equals(itemId)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    public List<Item> getInventory() {
+
+        return inventory;
     }
 }
